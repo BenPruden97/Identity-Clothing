591e416dbe4931fcc8a89198478832e103e7bf89
BenPruden97 <benpruden34@yahoo.com>
2021-12-05 13:01:52 +0000

    Customized admin

--- identity_clothing/urls.py
+++ identity_clothing/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('home.urls'))
+    path('', include('home.urls')),
 ] + static(settings.IMAGES_URL, document_root=settings.IMAGES_ROOT)
--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,35 @@ from django.contrib import admin
 from .models import Product, Category, ProductSize
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(ProductSize)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'product_id',
+        'name',
+        'description',
+        'category',
+        'price',
+        'product_rating',
+        'image',
+    )
+
+    ordering = ('product_id',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name'
+    )
+
+
+class ProductSizeAdmin(admin.ModelAdmin):
+    list_display = (
+        'product_size',
+        'product_size_name'
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(ProductSize, ProductSizeAdmin)

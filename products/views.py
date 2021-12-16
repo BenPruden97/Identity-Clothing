e54317b97a956113d9ebe2d306b691dd83093463
BenPruden97 <benpruden34@yahoo.com>
2021-12-16 19:00:04 +0000

    Started adding sorting clothing items functionality

--- products/views.py
+++ products/views.py
@@ -13,16 +13,34 @@ def all_products(request):
 
     products = Product.objects.all()
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
@@ -63,69 +81,3 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
-
-
-def mens_clothing(request):
-    """
-    Returns All Men's Clothing Items
-    """
-
-    products = Product.objects.all()
-    categories = None
-
-    if request.GET:
-        if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            products = products.filter(category__name__in=categories)
-            categories = Category.objects.filter(name__in=categories)
-
-    context = {
-        'products': products,
-        'current_categories': categories,
-    }
-
-    return render(request, 'products/products.html', context)
-
-
-def womens_clothing(request):
-    """
-    Returns All Women's Clothing Items
-    """
-
-    products = Product.objects.all()
-    categories = None
-
-    if request.GET:
-        if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            products = products.filter(category__name__in=categories)
-            categories = Category.objects.filter(name__in=categories)
-
-    context = {
-        'products': products,
-        'current_categories': categories,
-    }
-
-    return render(request, 'products/products.html', context)
-
-
-def sale_clothing(request):
-    """
-    Returns All Clothing Items Currently On Sale
-    """
-
-    products = Product.objects.all()
-    categories = None
-
-    if request.GET:
-        if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            products = products.filter(category__name__in=categories)
-            categories = Category.objects.filter(name__in=categories)
-
-    context = {
-        'products': products,
-        'current_categories': categories,
-    }
-
-    return render(request, 'products/products.html', context)

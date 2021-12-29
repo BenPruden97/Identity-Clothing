aa26b6d5394c4f4c1899e5bfe7910fa315498410
BenPruden97 <benpruden34@yahoo.com>
2021-12-29 17:01:51 +0000

    Added checkout success logic and template

--- checkout/models.py
+++ checkout/models.py
@@ -35,7 +35,7 @@ class Order(models.Model):
         Updates grand total each time the line item
         is added, accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_tota__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
@@ -60,7 +60,7 @@ class OrderLineItem(models.Model):
 
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems') 
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    product_size = models.CharField(max_length=2, null=True, blank=True,) # XXS, XS, S, M, L, XL, XXL
+    product_size = models.CharField(max_length=7, null=True, blank=True,) # XXS, XS, S, M, L, XL, XXL
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 

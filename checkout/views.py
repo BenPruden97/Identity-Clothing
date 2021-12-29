dd27b2ab6793a00f4fbeff801158d8be9dd97012
BenPruden97 <benpruden34@yahoo.com>
2021-12-29 16:09:22 +0000

    Added basic checkout functionality to payment form on checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -1,20 +1,44 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.context import bag_contents
+
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
-        message.error(request, "There is nothing in your shopping bag at the moment")
+        messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51KBiOEGQbvpK4zsWUeLMPQ02UXFMojEJJ7XcXvu44CAvc55nmvFh0uXOHOs7vxyHVoAJZYslHMQZk0p5JEM5oenN00PekWdMFw',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
--- identity_clothing/settings.py
+++ identity_clothing/settings.py
@@ -171,8 +171,13 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 IMAGES_URL = '/images/'
 IMAGES_ROOT = os.path.join(BASE_DIR, 'images')
 
+# Stripe
+
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'gbp'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

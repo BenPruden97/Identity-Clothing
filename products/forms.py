537c3b5e0ee2263372acb7c87dc85e26270a9873
BenPruden97 <benpruden34@yahoo.com>
2022-01-05 13:09:01 +0000

    Created add product view, url, template and add product form and buttons

--- products/forms.py
+++ products/forms.py
@@ -11,9 +11,9 @@ class ProductForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        catgories = Category.objects.all()
+        categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'product-form-input'
+            field.widget.attrs['class'] = 'product-management-form-input'

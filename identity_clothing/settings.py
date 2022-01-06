ca477f6b73eeb1f46a26e54267719e8061989ac2
BenPruden97 <benpruden34@yahoo.com>
2022-01-06 15:26:58 +0000

    Removed secret key and set debug

--- identity_clothing/settings.py
+++ identity_clothing/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3_lbgf#)+9z@&-wm$2&6%i89d0d^v$^t*9g*#5_#np07!)0h8s'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['identity-clothing.herokuapp.com', 'localhost']
 

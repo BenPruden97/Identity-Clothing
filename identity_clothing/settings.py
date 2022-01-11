ea3eb911644b41b98b8e97d5c9af68e6750b6ca3
BenPruden97 <benpruden34@yahoo.com>
2022-01-11 08:53:43 +0000

    Updated image urls for home page

--- identity_clothing/settings.py
+++ identity_clothing/settings.py
@@ -130,12 +130,10 @@ WSGI_APPLICATION = 'identity_clothing.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
-    print("loading postgres db....")
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
-    print("loading local db....")
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',

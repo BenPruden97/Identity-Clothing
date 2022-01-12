1a441fc0a01e21b23766460542045baa4b78faa2
BenPruden97 <benpruden34@yahoo.com>
2022-01-12 23:16:05 +0000

    Created index views to display popular items on home page. Styling swiper js element.

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from products.models import Product
+from django.db.models import Q
+
 # Create your views here.
 
 def index(request):
@@ -7,4 +10,18 @@ def index(request):
     View to return the index.html page
     """
 
-    return render(request, 'index.html')
+    best_seller_products = Product.objects.filter(Q(name="Men's Adidas Overhead Hoodie") |
+                                                  Q(name="Men's Star Wars Overhead Hoodie") |
+                                                  Q(name="Men's Black Nike T-Shirt") |
+                                                  Q(name="Women's White New York T-Shirt") |
+                                                  Q(name="Women's Adidas Overhead Hoodie") |
+                                                  Q(name="Women's Black Nike Shorts") |
+                                                  Q(name="Men's Black Nike Overhead Hoodie") |
+                                                  Q(name="Men's Blue Nike T-Shirt") |
+                                                  Q(name="Women's White Vogue T-Shirt"))
+
+    context = {
+        'best_seller_products': best_seller_products,
+    }
+
+    return render(request, 'index.html', context)

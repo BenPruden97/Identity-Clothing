1bdee8da7b5442d7d579e49f8dee973f45752217
BenPruden97 <benpruden34@yahoo.com>
2022-01-18 22:59:20 +0000

    Added checkout success page responsive design for all devices. Also added subtotal sale to bag tools

--- bag/templatetags/bag_tools.py
+++ bag/templatetags/bag_tools.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
     return price * quantity
+
+
+@register.filter(name='calc_subtotal_sale')
+def calc_subtotal_sale(get_sale_price, quantity):
+    return get_sale_price * quantity

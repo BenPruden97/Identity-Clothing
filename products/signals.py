021f534cfc7e9da5a1393f6c044868cbdee6e764
BenPruden97 <benpruden34@yahoo.com>
2022-01-27 23:52:54 +0000

    Fixed product reviews average rating to display when deleting product reviews. Added README file screenshots and styling errors for validation

--- products/signals.py
+++ products/signals.py
@@ -1,21 +1,20 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
-from django.db.models import Sum
-
 from .models import ProductReview
+
 
 @receiver(post_save, sender=ProductReview)
 def ben_find_averages(sender, instance, created, **kwargs):
 
     calculate_average_rating(instance)
 
+
 @receiver(post_delete, sender=ProductReview)
 def ben_averages(sender, instance, **kwargs):
     """
     Update product average_rating on rating_add_rating delete
     """
-    
     calculate_average_rating(instance)
 
 
@@ -27,11 +26,16 @@ def calculate_average_rating(instance):
     product_sum = 0
     product_count = 0
 
-    for review in current_product_reviews:
-        product_sum += review.review_add_rating
-        product_count += 1
+    if current_product_reviews:
+        for review in current_product_reviews:
+            product_sum += review.review_add_rating
+            product_count += 1
 
-    instance.product.average_rating = round(product_sum/product_count)
+        if product_sum > 0:
+            instance.product.average_rating = round(product_sum/product_count) or 0
+        else:
+            instance.product.average_rating == 0
+    else:
+        instance.product.average_rating = 0
 
-    print(instance.product.average_rating)
     instance.product.save()

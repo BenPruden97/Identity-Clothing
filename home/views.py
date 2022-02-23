b3a913461fbd7716ae06dba598941580cb498808
BenPruden97 <benpruden34@yahoo.com>
2022-02-23 20:31:11 +0000

    Fixing errors for code validation and CRUD functionality

--- home/views.py
+++ home/views.py
@@ -15,24 +15,15 @@ def index(request):
     View to return the index.html page
     """
 
-    best_seller_products = Product.objects.filter(Q(name="Men's Adidas\
-                                                  Overhead Hoodie") |
-                                                  Q(name="Men's Star Wars\
-                                                  Overhead Hoodie") |
-                                                  Q(name="Men's Black Nike\
-                                                  T-Shirt") |
-                                                  Q(name="Women's White New\
-                                                  York T-Shirt") |
-                                                  Q(name="Women's Adidas\
-                                                  Overhead Hoodie") |
-                                                  Q(name="Women's Black\
-                                                  Nike Shorts") |
-                                                  Q(name="Men's Black Nike\
-                                                  Overhead Hoodie") |
-                                                  Q(name="Men's Blue Nike\
-                                                  T-Shirt") |
-                                                  Q(name="Women's White\
-                                                  Vogue T-Shirt"))
+    best_seller_products = Product.objects.filter(Q(name="Men's Adidas Overhead Hoodie") |
+                                                  Q(name="Men's Star Overhead Hoodie") |
+                                                  Q(name="Men's Black Nike T-Shirt") |
+                                                  Q(name="Women's White New York T-Shirt") |
+                                                  Q(name="Women's Adidas Overhead Hoodie") |
+                                                  Q(name="Women's Black Nike Shorts") |
+                                                  Q(name="Men's Black Nike Overhead Hoodie") |
+                                                  Q(name="Men's Blue Nike T-Shirt") |
+                                                  Q(name="Women's White Vogue T-Shirt"))
 
     context = {
         'best_seller_products': best_seller_products,
